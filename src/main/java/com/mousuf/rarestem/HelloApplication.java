68f1be528aa3ad68395f19e85220e7d1d7ee72ef
Mousuf C Zaman <9200286+MousufCZ@users.noreply.github.com>
2023-04-27 18:25:28 +0100

    UPDATED: Environment set up with Maven Dependencies on a JavaFX Project file. ERR SOLVED: Dependency version control. - Mousuf C Zaman - 27-04-22

--- src/main/java/com/mousuf/rarestem/HelloApplication.java
+++ src/main/java/com/mousuf/rarestem/HelloApplication.java
@@ -4,11 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import lombok.Data;
 
 import java.io.IOException;
 
-public @Data class HelloApplication extends Application {
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
