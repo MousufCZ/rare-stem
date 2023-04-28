4e8ffb5d616a5170335f035f68877f7996fc49f0
Mousuf C Zaman <9200286+MousufCZ@users.noreply.github.com>
2023-04-28 14:42:03 +0100

    UPDATE: .env file on .gitingnore ERR SOLVED: - Mousuf C Zaman - 28-04-22

--- src/main/java/com/mousuf/rarestem/DatabaseConn.java
+++ src/main/java/com/mousuf/rarestem/DatabaseConn.java
@@ -9,15 +9,23 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseConn {
     public static void main( String[] args ) {
-
-        MongoClient client = MongoClients.create("mongodb+srv://mousufbyae:OVf32A0a72vVuH2v@cluster0.uuqglsd.mongodb.net/test");
+        MongoClient client = MongoClients.create("mongodb+srv://mousufbyae:k0VDzzU9vOGItt6n@cluster0.uuqglsd.mongodb.net/test");
         MongoDatabase db = client.getDatabase("rs-db");
         MongoCollection col = db.getCollection("rs-db");
 
-        Document sampleDoc = new Document("_id", "2").append("name", "Mousuf Chowdhury");
+        Document filter = new Document("_id", "4");
+        col.deleteOne(filter);
 
-        col.insertOne(sampleDoc);
+/*        Document filter = new Document("_id", "4");
+        Document update = new Document("$set", new Document("age", "35"));
+        col.updateOne(filter, update);*/
+
+/*        Document sampleDoc = new Document("_id", "4").append("age", "33");
+        col.insertOne(sampleDoc);*/
     }
 }
