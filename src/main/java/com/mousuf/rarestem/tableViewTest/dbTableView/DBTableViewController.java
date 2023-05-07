ae51c209d10a62592717f92a2c478c6c34a69c99
Mousuf C Zaman <9200286+MousufCZ@users.noreply.github.com>
2023-05-07 05:21:08 +0100

    UPDATED: The table view test is working when retriving data from MongoDB Collection using MVC model. ERR: Switch over Contoller code with Model code, update scenebuilder fx:controller=?. I was too tired. - MCZ - 06-05-22

--- src/main/java/com/mousuf/rarestem/tableViewTest/dbTableView/DBTableViewController.java
+++ src/main/java/com/mousuf/rarestem/tableViewTest/dbTableView/DBTableViewController.java
@@ -1,4 +1,60 @@
 package com.mousuf.rarestem.tableViewTest.dbTableView;
+import com.mousuf.rarestem.tableViewTest.Person;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mousuf.rarestem.tableViewTest.Person;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
 public class DBTableViewController {
+    private final MongoClient client;
+    private final MongoDatabase db;
+    private final MongoCollection<Document> col;
+
+    public DBTableViewController() {
+
+        // Connect to MongoDB
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        // Get the value of the API_KEY environment variable
+        String uri;
+        uri = dotenv.get("MONGODB_URI");
+        // Use the API key in your application
+        System.out.println("MongoDB URI: " + uri);
+
+        // create a MongoDB client
+        client = MongoClients.create(uri);
+
+        // get a database instance
+        db = client.getDatabase("rs-db");
+        System.out.println("Get Database successful.");
+
+
+        // get a collection instance
+        col = db.getCollection("person");
+        System.out.println("Get collection successful.");
+
+    }
+
+    public List<Person> getAllPersons() {
+        List<Person> persons = new ArrayList<>();
+
+        for (Document doc : col.find()) {
+            String fn = doc.getString("fn");
+            String ln = doc.getString("ln");
+            String o = doc.getString("o");
+
+            persons.add(new Person(fn, ln, o));
+        }
+
+        return persons;
+    }
+
+    public void close() {
+        client.close();
+    }
 }
--- src/main/java/com/mousuf/rarestem/tableViewTest/dbTableView/DBTableViewModel.java
+++ src/main/java/com/mousuf/rarestem/tableViewTest/dbTableView/DBTableViewModel.java
@@ -1,4 +1,50 @@
 package com.mousuf.rarestem.tableViewTest.dbTableView;
+import com.mousuf.rarestem.tableViewTest.Person;
+import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class DBTableViewModel {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class DBTableViewModel implements Initializable {
+    @FXML
+    private TableColumn<Person, String> fn;
+
+    @FXML
+    private TableColumn<Person, String> ln;
+
+    @FXML
+    private TableColumn<Person, String> o;
+
+    @FXML
+    public TableView<Person> table;
+
+    private DBTableViewController dbTableViewController;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fn.setCellValueFactory(new PropertyValueFactory<Person, String>("fn"));
+        ln.setCellValueFactory(new PropertyValueFactory<Person, String>("ln"));
+        o.setCellValueFactory(new PropertyValueFactory<Person, String>("o"));
+
+        dbTableViewController = new DBTableViewController();
+        List<Person> persons = dbTableViewController.getAllPersons();
+
+        if (table != null) {
+            System.out.println("FXCollections.observableArrayList(persons)");
+            table.setItems(FXCollections.observableArrayList(persons));
+        } else {
+            System.out.println("table is null");
+        }
+    }
+
+    public void onClose() {
+        dbTableViewController.close();
+    }
 }
